ff51f405a1bb2537d8e251b38b1693fe0d945c37
Karan-Bharti1 <karan.codes1@gmail.com>
2025-06-03 21:33:05 +0530

    chore: Established database connection

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express=require("express")
 const mongoose=require("mongoose")
 
+const app=express()
 const cors=require("cors")
 const dotenv=require("dotenv")
-const http=require('http')
 const authRoutes=require("./routes/auth")
 const RmsUser = require("./models/RmsUser")
+app.use(cors())
+app.use(express.json())
+dotenv.config()
+mongoose.connect(process.env.MONGODB).then(()=>console.log("Database connected successfully")).catch(error=>console.error(error))
+
+app.use("/auth",authRoutes);
+const PORT=process.env.PORT||5000
+
+app.get("/",async(req,res)=>{
+    res.status(200).json({message:"RMS Backend"})
+})
+app.listen(PORT,()=>{
+    console.log("App is running on the PORT: "+PORT)
+})
