568e721a149cca678d4293db6de73b2cfbb5622a
Karan-Bharti1 <karan.codes1@gmail.com>
2025-06-04 13:55:03 +0530

    feat: added api to create and get projects data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors=require("cors")
 const dotenv=require("dotenv")
 const authRoutes=require("./routes/auth")
 const RmsUser = require("./models/RmsUser")
+const RmsProject = require("./models/RmsProject")
 app.use(cors())
 app.use(express.json())
 dotenv.config()
@@ -16,6 +17,33 @@ const PORT=process.env.PORT||5000
 
 app.get("/",async(req,res)=>{
     res.status(200).json({message:"RMS Backend"})
+})
+app.post("/projects",async(req,res)=>{
+    try {
+        const data=req.body
+            if (!data) {
+      return res.status(400).json({ message: "No data provided" });
+    }
+
+    const newProject = new RmsProject(data);
+    const savedProject = await newProject.save();
+
+    res.status(201).json({ message: "Project created successfully", project: savedProject });
+    } catch (error) {
+        res.status(500).json({message:"Failed to post project data"})
+    }
+})
+app.get("/projects",async(req,res)=>{
+    try {
+        const data=await RmsProject.find().populate("managerId");
+        if(data ){
+res.status(200).json(data)
+        }else{
+            res.status(404).json({message:"Data not found"})
+        }
+    } catch (error) {
+         res.status(500).json({message:"Failed to get project data"})
+    }
 })
 app.listen(PORT,()=>{
     console.log("App is running on the PORT: "+PORT)
